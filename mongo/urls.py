d1f624d7cf72e719be7e1e95fcdea542788a313e
siddhant-soni <primeoptimus98@gmail.com>
2018-04-29 02:33:30 -0700

    Added Class-Based Views

--- mongo/urls.py
+++ mongo/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^read/$', views.read, name='read'),
-    url(r'^create/$', views.create, name='create'),
-    url(r'^update/$', views.update, name='update'),
-    url(r'^delete/$', views.delete, name='delete')
+    url(r'^read/$', views.ReadListView.as_view(), name='read'),
+    url(r'^create/$', views.CreateObjectView.as_view(), name='create'),
+    url(r'^update/(?P<pk>\d+)/$', views.UpdateObjectView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>\d+)/$', views.DeleteObjectView.as_view(), name='delete'),
 ]
--- mongo/views.py
+++ mongo/views.py
@@ -1,48 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Test
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
-def create(request):
-    if request.method == "POST":
-        text = request.POST.get('txt')
-        Test.objects.create(text=text)
-        return redirect('read')
-    return render(request, 'create.html', {})
+class CreateObjectView(CreateView):
+    model = Test
+    template_name_suffix = '_create_form'
+    fields = ['text']
+    success_url = '/read/'
 
 
-def read(request):
-    tests = Test.objects.all()
-    return render(request, 'show.html', {'tests': tests})
+class ReadListView(ListView):
+    model = Test
 
 
-def update(request):
-    if request.method == "POST":
-        id = request.POST.get('id')
-        text = request.POST.get('txt')
-        if id:
-            test = Test.objects.filter(id=id)
-            if test:
-                test.update(text=text)
-                return redirect('read')
-            else:
-                return render(request, 'update.html', {'error': 1})
-        else:
-            return render(request, 'update.html', {'error': 1})
-    else:
-        return render(request, 'update.html', {})
+class UpdateObjectView(UpdateView):
+    model = Test
+    template_name_suffix = '_update_form'
+    fields = ['text']
+    success_url = '/read/'
 
 
-def delete(request):
-    if request.method == "POST":
-        id = request.POST.get('id')
-        if id:
-            test = Test.objects.filter(id=id)
-            if test:
-                test.delete()
-                return redirect('read')
-            else:
-                return render(request, 'delete.html', {'error': 1})
-        else:
-            return render(request, 'delete.html', {'error': 1})
-    else:
-        return render(request, 'delete.html', {})
+class DeleteObjectView(DeleteView):
+    model = Test
+    success_url = '/read/'
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
